3b82ef45d1d627ed3ccb2b461ae0e261a644401f
Alex Rich <alex@Alexs-MacBook-Pro-5.local>
2018-10-09 22:09:22 -0700

    Add basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_204625) do
+ActiveRecord::Schema.define(version: 2018_10_10_050147) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_10_05_204625) do
     t.boolean "checked_out"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "display_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
