dd76039419dc6b2eeb7acbcbf29eaad3c1c6b71d
kasperkberg <KasperKBerg@169-231-129-87.wireless.ucsb.edu>
2018-10-12 12:10:37 -0700

    review-model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :category, required: false
-
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_062031) do
+ActiveRecord::Schema.define(version: 2018_10_12_185037) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2018_10_11_062031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "user_id"
+    t.boolean "anonymous"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
