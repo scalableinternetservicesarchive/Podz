eaf503119b47076e55d3ed9c4083df1b43d3a97c
Ryan Allen <rmallensb@gmail.com>
2018-10-12 12:44:37 -0700

    Initial item refactor, includes name->title, checked_out->available, price_hourly_usd, price_daily_usd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :checked_out)
+      params.require(:item).permit(:title, :description, :available, :price_hourly_usd, :price_daily_usd)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_062031) do
+ActiveRecord::Schema.define(version: 2018_10_12_193645) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,12 +20,14 @@ ActiveRecord::Schema.define(version: 2018_10_11_062031) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "description"
-    t.boolean "checked_out"
+    t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.float "price_hourly_usd"
+    t.float "price_daily_usd"
   end
 
   create_table "users", force: :cascade do |t|
