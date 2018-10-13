cd6604b5b809f019dd78516d33a0039d4615065d
Ryan Allen <rmallensb@gmail.com>
2018-10-13 14:55:26 -0700

    Review page now automatically populates user id

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,11 @@
 module ItemsHelper
   # Given a User id, return the display name of the User
-  def get_display_name(user_id)
-    @user = User.find(user_id)
-    @display_name = @user.display_name
+  def get_display_name(review)
+    if review.anonymous
+      @display_name = 'Anonymous'
+    else
+      @user = User.find(review.user_id)
+      @display_name = @user.display_name
+    end
   end
 end
