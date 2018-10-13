a97ea2da06d8ffef937560fc12a7c8b35cd79026
Ryan Allen <rmallensb@gmail.com>
2018-10-13 13:55:13 -0700

    Added sloppy view of reviews under item show page

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,7 @@
 module ItemsHelper
+  # Given a User id, return the display name of the User
+  def get_display_name(user_id)
+    @user = User.find(user_id)
+    @display_name = @user.display_name
+  end
 end
