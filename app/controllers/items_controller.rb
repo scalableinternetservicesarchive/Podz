36afc3e0c0b2da6cd74fabd10508dd5e41cbdc0e
Alex Rich <alexrich021@gmail.com>
2018-10-16 15:59:18 -0700

    Fix flash message on item destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,10 +64,8 @@ class ItemsController < ApplicationController
   # DELETE /items/1.json
   def destroy
     @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = "Item successfully destroyed"
+    redirect_to items_path
   end
 
   private
