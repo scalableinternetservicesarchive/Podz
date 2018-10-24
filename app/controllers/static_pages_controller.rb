ecba39194e5d36ad74b704dfb70ed983d62b6fbd
Alex Rich <alexrich021@gmail.com>
2018-10-23 21:13:56 -0700

    Add top users to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @items = get_top_items
+    @users = get_top_users
   end
 
   def about
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  # Returns the top n users based on ratings
+  def get_top_users(n: 10)
+    users = User.all.sort_by do |user|
+      user.items.sum do |item|
+        item.reviews.sum do |review|
+          review.rating
+        end
+      end
+    end
+    users[0..n]
+  end
+
 end
