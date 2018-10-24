cebf41847b534413c7dcdf4395a5e029acfdaadc
kasperkberg <KasperKBerg@Kaspers-MacBook-Pro.local>
2018-10-23 18:35:07 -0700

    user-reviews only editable by owner + some model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :category, required: false
   belongs_to :user,     required: false
   has_many :reviews
+  validates :title, length: {minimum: 1}
+  validates :description, length: {minimum: 1}
+  validates :price_hourly_usd, :inclusion => 0..999999
+  validates :price_daily_usd, :inclusion => 0..999999
 end
