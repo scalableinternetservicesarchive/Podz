e165a23927b9e2000c720ca769d341546d9169fc
Alex Rich <alexrich021@gmail.com>
2018-11-01 14:25:10 -0700

    Fix authenticate user test

--- test/integration/authenticated_actions_test.rb
+++ test/integration/authenticated_actions_test.rb
@@ -14,8 +14,6 @@ class AuthenticatedActionsTest < ActionDispatch::IntegrationTest
   end
 
   test "authenticated user should be able to make items" do
-    item = Item.new(title: "New Item", description: "This is a new item", price_hourly_usd: "20", price_daily_usd: "100")
-
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
@@ -23,8 +21,14 @@ class AuthenticatedActionsTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     get new_item_path
     assert_template 'items/new'
-    post items_path, params: { item: { title: item.title, description: item.description, category_id: item.category_id, price_hourly_usd: item.price_hourly_usd, price_daily_usd: item.price_daily_usd },
-                               condition: item.condition, user_lat: item.latitude, user_lng: item.longitude}
+    post items_path, params: { item: { title: "New Item",
+                                       description: "This is a new item",
+                                       category_id: 1,
+                                       price_hourly_usd: 20,
+                                       price_daily_usd: 100 },
+                               condition: "Good",
+                               user_lat: 0.0,
+                               user_lng: 0.0}
     follow_redirect!
     assert_template "items/show"
   end
