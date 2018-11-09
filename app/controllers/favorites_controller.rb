c67b2d51185477bc3136d103f6b9559bc4de8248
Alex Rich <alexrich021@gmail.com>
2018-11-09 10:07:08 -0800

    Ajaxify favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,14 @@
 class FavoritesController < ApplicationController
+  respond_to? :js
 
   def favorite
-    puts params
+    @item = Item.find_by(id: params[:item_id])
     Favorite.create!(user_id: current_user.id, item_id: params[:item_id])
-    redirect_to items_path
   end
 
   def unfavorite
-    puts params
+    @item = Item.find_by(id: params[:item_id])
     @favorite = Favorite.find_by(user_id: current_user.id, item_id: params[:item_id])
     @favorite.destroy!
-    redirect_to items_path
   end
 end
