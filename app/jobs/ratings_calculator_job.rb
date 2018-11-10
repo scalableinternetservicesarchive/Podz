d2a388903ce48304d0a10a7354288dced83f531e
Ryan Allen <rmallensb@gmail.com>
2018-11-09 20:12:56 -0800

    Fixed background job

--- app/jobs/ratings_calculator_job.rb
+++ app/jobs/ratings_calculator_job.rb
@@ -5,12 +5,12 @@ class RatingsCalculatorJob < ApplicationJob
     # Calculate top 10 Users
     users = User.all.sort_by { |user| user.rating * user.items.length }
     users.reverse!
-    users[0..10]
+    users = users[0...10]
 
     # Calculate top 10 Items
     items = Item.all.sort_by { |item| [item.rating * (item.reviews.length > 2 ? 1 : 0), item.reviews.length] }
     items.reverse!
-    items[0..10]
+    items = items[0...10]
 
     # If a top user ranking has changed, update
     index = 0
@@ -20,7 +20,7 @@ class RatingsCalculatorJob < ApplicationJob
       end
       index += 1
     end
-    while index < 10
+    while index < 10 && index < users.count
       TopUser.create(user_id: users[index].id)
       index += 1
     end
@@ -33,7 +33,7 @@ class RatingsCalculatorJob < ApplicationJob
       end
       index += 1
     end
-    while index < 10
+    while index < 10 && index < items.count
       TopItem.create(item_id: items[index].id)
       index += 1
     end
