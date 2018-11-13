17fe7ab5e388453f721ea08364c8c2ce561f8543
Ryan Allen <rmallensb@gmail.com>
2018-11-13 13:28:29 -0800

    Added guard for top_items as well, also added transactions to rating job

--- app/jobs/ratings_calculator_job.rb
+++ app/jobs/ratings_calculator_job.rb
@@ -10,25 +10,31 @@ class RatingsCalculatorJob < ApplicationJob
       users.reverse!
       users = users[0...10]
 
-      # Calculate top 10 Items
-      items = Item.all.sort_by { |item| [item.rating * (item.reviews.length > 2 ? 1 : 0), item.reviews.length] }
-      items.reverse!
-      items = items[0...10]
-
       # If a top user ranking has changed, update
-      index = 0
-      TopUser.all.each do |user|
-        unless user.user_id == users[index].id
-          user.update(user_id: users[index].id)
+      ActiveRecord::Base.transaction do
+        index = 0
+        TopUser.all.each do |user|
+          unless user.user_id == users[index].id
+            user.update(user_id: users[index].id)
+          end
+          index += 1
         end
-        index += 1
+        while index < 10 && index < users.count
+          TopUser.create(user_id: users[index].id)
+          index += 1
+        end
       end
-      while index < 10 && index < users.count
-        TopUser.create(user_id: users[index].id)
-        index += 1
-      end
+    end
 
-      # If a top item ranking has changed, update
+    return unless ActiveRecord::Base.connection.table_exists? 'top_items'
+
+    # Calculate top 10 Items
+    items = Item.all.sort_by { |item| [item.rating * (item.reviews.length > 2 ? 1 : 0), item.reviews.length] }
+    items.reverse!
+    items = items[0...10]
+
+    # If a top item ranking has changed, update
+    ActiveRecord::Base.transaction do
       index = 0
       TopItem.all.each do |item|
         unless item.item_id == items[index].id
