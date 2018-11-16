b9b774c6994b1f4afeca5e0443d84239f8402885
Alex Rich <alexrich021@gmail.com>
2018-11-16 12:46:06 -0800

    Change thread count and process count

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+workers 3
+threads 5, 5
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
