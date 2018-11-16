21a8e9d3d541ffa7dcfb3a1c56eb2d41de8a5d10
Alex Rich <alexrich021@gmail.com>
2018-11-16 14:17:47 -0800

    Optimize rating queries

--- app/jobs/ratings_calculator_job.rb
+++ app/jobs/ratings_calculator_job.rb
@@ -4,44 +4,58 @@ class RatingsCalculatorJob < ApplicationJob
   def perform(*args)
     # Check if table exists (necessary to pass Travis CI checks)
     if ActiveRecord::Base.connection.table_exists? 'top_users'
-      # Calculate top 10 Users
-      users = User.all.sort_by { |user| user.rating * user.items.length }
-      users.reverse!
-      users = users[0...10]
+      sql = "SELECT users.id as id, count(rating) as count, sum(rating) as sum
+             FROM users JOIN items ON (users.id = items.user_id)
+                        JOIN reviews ON (items.id = reviews.item_id)
+             GROUP BY users.id
+             ORDER BY sum DESC"
+
+      users = ActiveRecord::Base.connection.execute sql
+      top_users = users[0...10]
+
+      # # Calculate top 10 Users
+      # users = User.all.sort_by { |user| user.rating * user.items.length }
+      # users.reverse!
+      # users = users[0...10]
 
       # If a top user ranking has changed, update
       ActiveRecord::Base.transaction do
         index = 0
         TopUser.all.each do |user|
-          unless user.user_id == users[index].id
-            user.update(user_id: users[index].id)
+          unless user.user_id == top_users[index]["id"]
+            puts "Updating"
+            user.update(user_id: top_users[index]["id"])
           end
           index += 1
         end
-        while index < 10 && index < users.count
-          TopUser.create(user_id: users[index].id)
+        while index < 10 && index < top_users.length
+          TopUser.create(user_id: top_users[index]["id"])
           index += 1
         end
       end
     end
 
     if ActiveRecord::Base.connection.table_exists? 'top_items'
-      # Calculate top 10 Items
-      items = Item.all.sort_by { |item| [item.rating * (item.reviews.length > 2 ? 1 : 0), item.reviews.length] }
-      items.reverse!
-      items = items[0...10]
+
+      sql = "SELECT items.id as id, count(rating) as count, sum(rating) as sum
+             FROM items JOIN reviews ON (items.id = reviews.item_id)
+             GROUP BY items.id
+             ORDER BY sum DESC"
+
+      items = ActiveRecord::Base.connection.execute sql
+      top_items = items[0...10]
 
       # If a top item ranking has changed, update
       ActiveRecord::Base.transaction do
         index = 0
         TopItem.all.each do |item|
-          unless item.item_id == items[index].id
-            item.update(item_id: items[index].id)
+          unless item.item_id == top_items[index]["id"]
+            item.update(item_id: top_items[index]["id"])
           end
           index += 1
         end
-        while index < 10 && index < items.count
-          TopItem.create(item_id: items[index].id)
+        while index < 10 && index < top_items.length
+          TopItem.create(item_id: top_items[index]["id"])
           index += 1
         end
       end
