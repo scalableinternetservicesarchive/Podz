41c60691ebf084e8a8f2aff393510d415eada4d8
Ryan Allen <rmallensb@gmail.com>
2018-11-20 16:20:37 -0800

    Rolled back item page optimization

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
       @items = @items.where('LOWER(title) LIKE :keyword OR LOWER(description) LIKE :keyword', { keyword: "%#{@keyword.downcase}%" })
     end
 
+    unless current_user.nil?
+      @items = @items.where('user_id != ?', current_user.id)
+    end
+
     @items_free = @items.where(available: true).includes(:category).paginate(per_page: 20, page: params[:page])
     @items_rented = @items.where(available: false).includes(:category).paginate(per_page: 20, page: params[:page])
   end
